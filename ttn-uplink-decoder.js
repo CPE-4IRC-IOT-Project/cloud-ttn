c0bcdab665f1aa5f0d7ef31e336c252e090f2bda
Valentiiine <valentine.paul@cpe.fr>
2026-03-03 14:56:19 +0100

    décodeur TTN à jour

--- ttn-uplink-decoder.js
+++ ttn-uplink-decoder.js
@@ -36,7 +36,6 @@ function decodeUplink(input) {
         : (msgType === 0x02 ? "occupancy_changed" : (msgType === 0x03 ? "temperature" : "unknown")),
     node_id: nodeId,
     flags: flags,
-    low_light: (flags & 0x01) !== 0,
     counter: u32be(bytes, 8),
     uptime_s: u32be(bytes, 12),
   };
@@ -54,6 +53,7 @@ function decodeUplink(input) {
   return {
     data: {
       ...base,
+      low_light: (flags & 0x01) !== 0,
       luma: bytes[4],
       occupied: bytes[5],
       stable_count: bytes[6],
